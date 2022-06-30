276a3c315dc7a0245cac21f69b15cff5de9c6e4d
Rodrigo Costa <costardgg@gmail.com>
2022-06-29 23:54:09 -0300

    Funcionalidade botao Criar Quizz

---
+++ script.js
@@ -0,0 +1,8 @@
+function CriarQuiz(elemente) {
+    let Fechartela1 = document.querySelector(".Primeiratela");
+    let AbrirTela3 = document.querySelector(".Terceiratela");
+    Fechartela1.classList.add("escondido");
+    AbrirTela3.classList.remove("escondido");
+}
+
+
