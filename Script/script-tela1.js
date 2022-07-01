fb417812eda18bb68f0843896c00eb3df741cebd
Rodrigo Costa <costardgg@gmail.com>
2022-07-01 20:03:05 -0300

    Apresentaçao todos os quizzes

--- Script/script-tela1.js
+++ Script/script-tela1.js
@@ -1,3 +1,6 @@
+PegarQuizz();
+
+
 function CriarQuiz() {
     let Fechartela1 = document.querySelector(".Primeiratela");
     let AbrirTela3 = document.querySelector(".Terceiratela");
@@ -5,4 +8,22 @@ function CriarQuiz() {
     AbrirTela3.classList.remove("escondido");
 }
 
+function PegarQuizz(){
+    const requerimento= axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+    requerimento.then(popularQuizzes);
+}
 
+function popularQuizzes(resposta){
+const info = resposta.data;
+let quizzes = document.querySelector('.TodosQuizzes');
+quizzes.innerHTML= "";
+
+for (i=0; i < info.length; i++ ){
+
+quizzes.innerHTML += `<div class="quizz">
+ <img src="${info[i].image}" class="${info[i].id}">
+ <div class="Titulo">${info[i].title} </div>
+ </div>`
+ }
+
+}
